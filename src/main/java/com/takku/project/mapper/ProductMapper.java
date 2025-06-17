21e1cd82b0a3848a489096bfd04459ead41ea5a2
dnjswjd222 <bnm673@naver.com>
2025-06-17 19:09:26 +0900

    .

--- src/main/java/com/takku/project/mapper/ProductMapper.java
+++ src/main/java/com/takku/project/mapper/ProductMapper.java
@@ -7,13 +7,13 @@ import com.takku.project.domain.ProductDTO;
 public interface ProductMapper {
 
 	// 1. ��ǰ ���
-	int insertProduct(ProductDTO productVO);
+	int insertProduct(ProductDTO product);
 
     // 2. ���� ID�� ��ǰ ��� ��ȸ
     List<ProductDTO> getProductByStoreId(Integer storeId);
 
     // 3. ��ǰ ����
-    int updateProduct(ProductDTO productVO);
+    int updateProduct(ProductDTO product);
 
     // 4. ��ǰ ����
     int deleteProduct(Integer productId);
