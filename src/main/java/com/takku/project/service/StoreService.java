5c66aa9de72c8e707b812e540aaa35b823c2ac0e
dnjswjd222 <bnm673@naver.com>
2025-06-17 19:03:31 +0900

    .

---
+++ src/main/java/com/takku/project/service/StoreService.java
@@ -0,0 +1,39 @@
+package com.takku.project.service;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.takku.project.domain.StoreDTO;
+import com.takku.project.mapper.StoreMapper;
+
+public class StoreService implements StoreMapper{
+
+	@Autowired
+	SqlSession sqlSession;
+	String namespace = "com.takku.project.mapper.StoreMapper.";
+	
+	@Override
+	public int insertStore(StoreDTO store) {
+		int result = sqlSession.insert(namespace + "insertStore", store);
+		return result;
+	}
+
+	@Override
+	public StoreDTO selectStoreById(Integer storeId) {
+		StoreDTO store = sqlSession.selectOne(namespace + "selectStoreById", storeId);
+		return store;
+	}
+
+	@Override
+	public int updateStore(StoreDTO store) {
+		int result = sqlSession.update(namespace + "updateStore", store);
+		return result;
+	}
+
+	@Override
+	public int deleteStore(Integer storeId) {
+		int result = sqlSession.delete(namespace + "deleteStore", storeId);
+		return result;
+	}
+
+}
---
+++ src/main/java/com/takku/project/service/UserService.java
@@ -0,0 +1,40 @@
+package com.takku.project.service;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.takku.project.domain.UserDTO;
+import com.takku.project.mapper.UserMapper;
+
+public class UserService implements UserMapper{
+
+	@Autowired
+	SqlSession sqlSession;
+	String namespace = "com.takku.project.mapper.UserMapper.";
+	
+	@Override
+	public int insertUser(UserDTO user) {
+		int result = sqlSession.insert(namespace + "insertUser", user);
+		return result;
+	}
+
+	@Override
+	public UserDTO selectByPhone(String phone) {
+		UserDTO user = sqlSession.selectOne(namespace + "selectByPhone", phone);
+		return user;
+	}
+
+	@Override
+	public UserDTO selectByUserId(Integer userId) {
+		UserDTO user = sqlSession.selectOne(namespace + "selectByUserId", userId);
+		return user;
+	}
+
+	@Override
+	public int updateUser(UserDTO user) {
+		int result = sqlSession.update(namespace + "updateUser", user);
+		return result;
+	}
+
+	
+}
