1e16f3e575f98d6d214c63b4d0c7524999f8456b
heejung003 <heejung003@gmail.com>
2025-06-19 15:32:28 +0900

    ♻️ refactor: ProductControllerTest 중복 코드 제거

--- src/main/java/com/takku/project/controllerTest/ProductControllerTest.java
+++ src/main/java/com/takku/project/controllerTest/ProductControllerTest.java
@@ -17,11 +17,9 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
